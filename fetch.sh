2cd23c833035e13abdbd0d7ad49ffb2115162e7e
Scott Fleming <scott.s.fleming@gmail.com>
2019-11-25 15:54:06 -0600

    Code cleanup and README update

--- fetch.sh
+++ fetch.sh
@@ -1,29 +1,26 @@
 #!/bin/sh
-
+#=================================================================
 # OmniTube v1.0 -- Scott Fleming
 # scott@omnicommander.com
-# PI_UID and URL are set as global variables in /etc/environment
-# ===============================================================
-# URL       = server api
-# PI_UID    = Unique identifier of PI machine making request.
-#----------------------------------------------------------------
+#=================================================================
 
 # Fetch json array of youtubeID's passed with serverID 
-echo "query: ${URL}${PI_UID}"
+echo "Executing Query: ${URL}${PI_UID}"
 
+# cURL request, save to json/file.json 
 curl -s -H "Content-Type: application/json" -X GET "${URL}${PI_UID}" -o json/file.json
 
 # use jq to parse array of json and load into arr
 arr=( $(jq -r '.[].youtube_id' json/file.json) )
 
-echo "${#arr[@]} assets to download."
-echo "clearing ./video directory"
+echo "${PI_UID} currently has ${#arr[@]} asset files to download"
+echo "Flushing video directory..."
 rm ./video/*
 
-# iterate arr and issue youtube-dl command for the id
+# iterate array and issue youtube-dl command for the id
 for i in "${!arr[@]}"
 do
-    echo "saving .video/${i}_${PI_UID}_${arr[$i]}.mp4"
+    echo "youtube-dl: ${i}_${PI_UID}_${arr[$i]}.mp4"
      
      youtube-dl --format mp4 --quiet --no-warnings --ignore-errors --ignore-config  --prefer-ffmpeg https://www.youtube.com/watch?v=${arr[$i]} -o "./video/${i}_${PI_UID}_${arr[$i]}.mp4"
 
