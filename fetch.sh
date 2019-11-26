e5c3af791ef5c9ef57f540b696088346c8af4938
Scott Fleming <scott@omnicommander.com>
2019-11-26 11:18:47 -0600

    updated fetch.sh to handle dirs and system calls.

--- fetch.sh
+++ fetch.sh
@@ -10,7 +10,7 @@ mkdir -p ./json
 
 
 # Fetch json array of youtubeID's passed with serverID 
-echo "Executing Query: ${URL}${PI_UID}"
+echo "Executing query for ID: ${PI_UID}."
 
 # cURL request, save to json/file.json 
 curl -s -H "Content-Type: application/json" -X GET "${URL}${PI_UID}" -o json/file.json
@@ -19,20 +19,21 @@ curl -s -H "Content-Type: application/json" -X GET "${URL}${PI_UID}" -o json/fil
 arr=( $(jq -r '.[].youtube_id' json/file.json) )
 
 echo "${PI_UID} currently has ${#arr[@]} asset files to download."
-#echo "Flushing video directory..."
+echo
 
 rm -f ./video/*
 
 # iterate array and issue youtube-dl command for the id
 for i in "${!arr[@]}"
 do
-    echo 
+   
     echo "executing youtube-dl: ${i}_${PI_UID}_${arr[$i]}.mp4..."
      
      youtube-dl --format mp4 --no-warnings --ignore-errors --ignore-config  --prefer-ffmpeg https://www.youtube.com/watch?v=${arr[$i]} -o "./video/${i}_${PI_UID}_${arr[$i]}.mp4"
 
+    echo
 done
 
 # Run the looper script
-#  bash looper.sh
+bash looper.sh
 
