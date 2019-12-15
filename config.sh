2428c7ec4db38ebc28823ae96695753bb041df65
Scott Fleming <scott@omnicommander.com>
2019-12-15 12:27:11 -0600

    looper and fetch script updates

--- config.sh
+++ config.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-TITLE="OmniTube v1.2a by Scott Fleming"
+TITLE=" "
 LOGOPATH="/home/pi/pi-tube/img/logo.png"
 VIDEOPATH="/home/pi/pi-tube/video/"
 fetchLog="/home/pi/pi-tube/fetch.log"
+# TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
 TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
 URL="http://34.193.150.151/pi_callHome.php?id="
 PI_UID="OC_OMNITUBE_1"
