a0f7eaf1d932d37c36d5f49c325723a3532484cb
hawkwynd <hawkwynd@gmail.com>
2019-12-16 15:34:00 -0600

    Changed config.sh paths for mkdir creation Changed fetch to be run as sudo, and perform extra functions Updated install script

--- config.sh
+++ config.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 TITLE="OmniTube"                                    # displays on lower right corner
 LOGOPATH="/home/pi/pi-tube/img/logo.png"            # omnicommander logo image 
-JSONPATH="/home/pi/pi-tube/json/file.json"          # path for json file from server.
-VIDEOPATH="/home/pi/pi-tube/video/"                 # path for fetch to store video mp4 files
+JSONPATH="/home/pi/pi-tube/json"          # path for json file from server.
+VIDEOPATH="/home/pi/pi-tube/video"                 # path for fetch to store video mp4 files
 fetchLog="/home/pi/pi-tube/fetch.log"               # path for logging 
 TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`               # timestamp formatting for logger
 URL="http://34.193.150.151/pi_callHome.php?id="     # url to server for requests
